a14e33df3331eac9b6824fed700e7d0607298603
Uviloc <60887495+Uviloc@users.noreply.github.com>
2026-09-07 13:24:22 +0200

    Added BG item (unpublished)

--- projects/list.js
+++ projects/list.js
@@ -1,10 +1,11 @@
 // List of all project HTML files
 window.PROJECT_FILES = [
-    "Pixel_Art/project.html",                       //pos 0
-    "Ontwaken_van_het_Stille_Woud/project.html",    //pos +1
-    "Into_The_Depth/project.html",                  //pos +2
+    //"BoekenGilde_Internship/project.html",          //pos 0
+    "Pixel_Art/project.html",                       //pos +1
+    "Ontwaken_van_het_Stille_Woud/project.html",    //pos +2
+    "Into_The_Depth/project.html",                  //hidden
     "Glow/project.html",                            //hidden
-    "Wave_Function_Collapse/project.html",          //hidden
+    //"Wave_Function_Collapse/project.html",          //hidden
     "Witchraft/project.html",                       //hidden
     "Procedural_Road_Gen/project.html",             //pos -2
     "Allus_Internship/project.html"                 //pos -1
@@ -21,6 +22,7 @@ window.TAG_ORDER = [
     "Pixel Art",
     "Unity",
     "Unreal",
+    "Figma",
     "WinForms",
     "C#",
     "Java"
